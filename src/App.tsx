d2d6268b64f63d00f23f6b06d8ef071e02da7b84
jithin krishna <jithin@inloops.in>
2025-06-12 23:33:30 +0530

    react-mini-app

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <div className= ' h-screen flex justify-center font-semibold items-center font-sans text-3xl'>Minimal React App</div>
+  )
+}
+
+export default App
