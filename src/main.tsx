1330b3ccb53c720221f0ab9cb0cc0631b8ef7a3d
devhousejithin <124678902+devhousejithin@users.noreply.github.com>
2025-07-20 03:04:26 +0530

    added frondend

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { SidebarProvider } from "@/components/ui/sidebar.tsx"
+import { AuthProvider } from './context/AuthContext.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <AuthProvider>
+        <SidebarProvider>
+          <App />
+        </SidebarProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
 )
